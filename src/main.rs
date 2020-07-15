b3954c25d5a66b6f6e611ef825f7c2b4337ceaf9
n01e0 <noleo@vanillabeans.mobi>
2020-07-15 15:47:28 +0900

    openしたいじゃん。

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate clap;
+extern crate opener;
 mod emoji;
 
 fn main() {
@@ -13,21 +14,32 @@ fn main() {
             (@arg back_color: -b --back_color +takes_value "background color(RGBA)")
             (@arg text: +required "emoji text")
             (@arg out: -o --outpath +takes_value "output path(default is $HOME/emoji)")
+            (@arg open: --open "xdg-open")
         ).get_matches();
 
     if matches.is_present("font_list") {
         emoji::font_list();
     } else {
-
+        let text = matches.value_of("text").unwrap();
         let mut genelater = emoji::Generator::new();
         genelater
             .font(matches.value_of("font").unwrap_or("notosans-mono-bold"))
             .color(matches.value_of("color").unwrap_or("EC71A1FF"))
             .back_color(matches.value_of("back_color").unwrap_or("00000000"))
-            .text(matches.value_of("text").unwrap());
+            .text(text);
 
         if let Err(e) = genelater.gen(matches.value_of("out")) {
             eprintln!("{:?}", e);
+            std::process::exit(1);
+        }
+
+        let filename = if let Some(name) = matches.value_of("out") {
+            name.to_string()
+        } else {
+            format!("{}/emoji/{}.png", std::env::var("HOME").unwrap(), text)
+        };
+        if matches.is_present("open") {
+            opener::open(filename).expect("Can't open file");
         }
     }
 }
